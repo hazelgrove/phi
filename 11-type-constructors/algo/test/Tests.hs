d209b789e0dad357e0e456e856c608c3880073aa
hejohns <hejohns@umich.edu>
2022-02-14 23:12:08 -0500

    what a waste of time fiddling w/ (.)/flip...

--- 11-type-constructors/algo/test/Tests.hs
+++ 11-type-constructors/algo/test/Tests.hs
@@ -1,11 +1,18 @@
 import Test.HUnit
-import Algo
+import Algo hiding (tequiv)
+import qualified Algo (tequiv)
 
 main = runTestTTAndExit tests
 
 tests :: Test
 tests = TestList
-  [ tequiv Nil Bse Bse Type ~?= True
-  , tequiv Nil Bse Bse (S Type Bse) ~?= True
-  , tequiv Nil Bse Bse (Π "t" Type (S Type (TVar "t"))) ~?= False
+-- in a somewhat increasing order of complexity
+  [ Nil |- tequiv Bse Bse Type ~?= True
+  , Nil |- tequiv Bse Bse (S Type Bse) ~?= True
+  , Nil |- tequiv Bse Bse (Π "t" Type (S Type (TVar "t"))) ~?= False
   ]
+
+aΓ |- f = f aΓ
+
+-- tequiv τ1 τ2 κ = \aΓ -> Algo.tequiv aΓ τ1 τ2 κ
+tequiv = ((.).(.) $ flip).((.) flip).flip $ Algo.tequiv
