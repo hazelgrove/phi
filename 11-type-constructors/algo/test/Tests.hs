da687e02b80ae6f44b8c658f4863ce388802a137
hejohns <hejohns@umich.edu>
2022-03-01 00:07:43 -0500

    hindent/various cosmetics

--- 11-type-constructors/algo/test/Tests.hs
+++ 11-type-constructors/algo/test/Tests.hs
@@ -1,18 +1,25 @@
-import Test.HUnit
+{-# LANGUAGE PartialTypeSignatures #-}
+
 import Algo hiding (tequiv)
 import qualified Algo (tequiv)
+import Test.HUnit
 
+main :: IO ()
 main = runTestTTAndExit tests
 
 tests :: Test
-tests = TestList
--- in a somewhat increasing order of complexity
-  [ Nil |- tequiv Bse Bse Type ~?= True
-  , Nil |- tequiv Bse Bse (S Type Bse) ~?= True
-  , Nil |- tequiv Bse Bse (Π "t" Type (S Type (TVar "t"))) ~?= False
-  ]
+tests =
+  TestList
+    [ Nil |- tequiv Bse Bse Type ~?= True
+    , Nil |- tequiv Bse Bse (S Type Bse) ~?= True
+    , Nil |- tequiv Bse Bse (Π "t" Type (S Type (TVar "t"))) ~?= False
+    ]
 
+-- in a somewhat increasing order of complexity
+(|-) :: _
 aΓ |- f = f aΓ
 
 -- tequiv τ1 τ2 κ = \aΓ -> Algo.tequiv aΓ τ1 τ2 κ
-tequiv = ((.).(.) $ flip).((.) flip).flip $ Algo.tequiv
+-- since application precedence is so high (and can't be competed against)
+tequiv :: _
+tequiv = ((.) . (.) $ flip) . ((.) flip) . flip $ Algo.tequiv
