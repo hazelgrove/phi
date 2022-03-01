6973fce5a8630c9557ea23259db3f8ee91e66a70
hejohns <hejohns@umich.edu>
2022-02-28 21:20:49 -0500

    finish αRename
    also we're using hindent now!
    The broken autoindent (well, broken on haskell, works fine for C) was
    driving me crazy

--- 11-type-constructors/algo/src/Common.hs
+++ 11-type-constructors/algo/src/Common.hs
@@ -1,8 +1,12 @@
+-- NOTE: we alwys assume programs are α-renamed so no shadowing occurs
 module Common where
 
 type TID = String
+
 type HID = Int
+
 type TAssump = (TID, Knd)
+
 type HAssump = (HID, Knd)
 
 data Typ
@@ -10,7 +14,9 @@ data Typ
   | Bse
   | Typ :⊕ Typ
   | ETHole HID
-  | NETHole
+  | NETHole HID Typ
+  | Tλ TID Knd Typ
+  | TAp Typ Typ
   deriving (Eq, Show)
 
 data Knd
@@ -19,3 +25,30 @@ data Knd
   | S Knd Typ
   | Π TID Knd Knd
   deriving (Eq, Show)
+
+class Rewrite a
+  {-
+   - okay, not exactly an alpha-conversion,
+   - but replaces all instances of a variable with another
+   -}
+  where
+  αRename :: TID -> TID -> a -> a
+
+instance Rewrite Typ where
+  αRename t'' t' (TVar t)
+    | t' == t = TVar t''
+    | otherwise = TVar t
+  αRename t'' t' (τ1 :⊕ τ2) = (αRename t'' t' τ1) :⊕ (αRename t'' t' τ2)
+  αRename t'' t' (NETHole u τ) = NETHole u (αRename t'' t' τ)
+  αRename t'' t' (Tλ t κ τ)
+    | t' == t = Tλ t'' (αRename t'' t' κ) (αRename t'' t' τ)
+    | otherwise = Tλ t (αRename t'' t' κ) (αRename t'' t' τ)
+  αRename t'' t' (TAp τ1 τ2) = TAp (αRename t'' t' τ1) (αRename t'' t' τ2)
+  αRename _ _ τ = τ
+
+instance Rewrite Knd where
+  αRename t'' t' (Π t κ1 κ2)
+    | t' == t = Π t'' (αRename t'' t' κ1) (αRename t'' t' κ2)
+    | otherwise = Π t (αRename t'' t' κ1) (αRename t'' t' κ2)
+  αRename t'' t' (S κ τ) = S (αRename t'' t' κ) (αRename t'' t' τ)
+  αRename _ _ κ = κ
