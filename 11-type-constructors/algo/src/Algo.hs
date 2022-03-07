a73675fe29d34d4b0d6d9b8cff119e066719812f
hejohns <hejohns@umich.edu>
2022-03-07 13:33:11 -0500

    revert most of last couple commits (see last commit)
    (does not build)

--- 11-type-constructors/algo/src/Algo.hs
+++ 11-type-constructors/algo/src/Algo.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE TypeFamilies #-}
 #define LINE (show (__LINE__ :: Integer))
 module Algo
   ( module Algo
@@ -12,56 +11,84 @@ import Common
 import Ctx
 
 import Control.Exception.Base
-import Data.Kind qualified as DK
 import Data.Maybe
 import Debug.Trace
 
-class Canon a
-  -- C for Canonical
-  where
-  data C a :: DK.Type
-  canon :: Ctx -> a -> Maybe (C a)
+tequiv :: Ctx -> Typ -> Typ -> Knd -> Bool
+tequiv aΓ τ1 τ2 κ =
+  isJust
+    (do ωτ1 <- canon aΓ τ1
+        ωτ2 <- canon aΓ τ2
+        ωκ <- canon aΓ κ
+        tequiv' aΓ ωτ1 ωτ2 ωκ |>> Just ())
 
--- C Typ only has variables if they are base types (do not have singleton kind)
--- TAp s are β reduced as much as possible
--- Tλ s are ``values'' (we don't canon the body)
+tequiv' :: Ctx -> Typ -> Typ -> Knd -> Bool
+tequiv' _ Bse Bse Type = True
+tequiv' aΓ (ωτ1 :⊕ ωτ2) (ωτ3 :⊕ ωτ4) Type =
+  (tequiv aΓ ωτ1 ωτ3 Type) && (tequiv aΓ ωτ2 ωτ4 Type)
+tequiv' aΓ (ETHole u) (ETHole u') κ = isJust (do u == u' |>> lookupH aΓ u)
+tequiv' aΓ (NETHole u1 τ1) (NETHole u2 τ2) κ =
+  isJust
+    (do _ <- u1 == u2 |>> lookupH aΓ u1
+        assert (τ1 ≡ τ2) $ Just ())
+tequiv' aΓ ωτ1@(Tλ _ _ _) ωτ2@(Tλ _ _ _) (Π t κ1 κ2) =
+  tequiv (aΓ ⌢ (t, κ1)) (TAp ωτ1 $ TVar t) (TAp ωτ2 $ TVar t) κ2
+tequiv' aΓ ωτ1 ωτ2 (S ωκ ωτ3) = tequiv aΓ ωτ1 ωτ2 ωκ && tequiv aΓ ωτ1 ωτ3 ωκ
+tequiv' _ _ _ _ = False
+
+kequiv :: Ctx -> Knd -> Knd -> Bool
+kequiv aΓ κ1 κ2 =
+  isJust
+    (do ωκ1 <- canon aΓ κ1
+        ωκ2 <- canon aΓ κ2
+        kequiv' aΓ ωκ1 ωκ2 |>> Just ())
+
+kequiv' :: Ctx -> Knd -> Knd -> Bool
+kequiv' aΓ κ@(Π t _ _) κ'@(Π t' _ _) =
+  let t'' = fresh2 t t'
+   in let (Π _ κ1 κ2) = αRename t'' t κ
+       in let (Π _ κ3 κ4) = αRename t'' t' κ'
+           in (kequiv aΓ κ1 κ3) && (kequiv (aΓ ⌢ (t'', κ1)) κ2 κ4)
+kequiv' aΓ (S κ1 τ1) (S κ2 τ2) = (kequiv aΓ κ1 κ2) && (tequiv aΓ τ1 τ2 κ1)
+kequiv' _ κ1 κ2 = κ1 ≡ κ2
+
+-- (didn't define a seperate datatype since more symbols would clash and I'm
+-- still changing a lot of stuff)
+class Canon a where
+  canon :: Ctx -> a -> Maybe a
+
 instance Canon Typ where
-  newtype C Typ = CTyp{getTyp :: Typ}
-                  deriving (Eq, Show, Rewrite)
-  canon aΓ τ@(TVar t) = do
+  canon aΓ (TVar t) = do
     γκ <- lookupT aΓ t
     case γκ of
       S κ' τ' -> canon aΓ τ'
-      Type -> Just . CTyp $ τ
+      Type -> Just $ TVar t
       KHole -> error "This should not happen"
-      Π _ _ _ -> do
-        ηExpand aΓ τ
-  canon _ Bse = Just . CTyp $ Bse
+      Π t' κ1 κ2 -> error "???"
+  canon _ Bse = Just Bse
   canon aΓ (τ1 :⊕ τ2) = do
     ωτ1 <- canon aΓ τ1
     ωτ2 <- canon aΓ τ2
-    (wfak aΓ (getTyp ωτ1) Type) && (wfak aΓ (getTyp ωτ2) Type) &>> Just . CTyp $ getTyp ωτ1 :⊕ getTyp ωτ2
+    return $ ωτ1 :⊕ ωτ2
   canon aΓ τ@(ETHole u) = do
     _ <- lookupH aΓ u
-    Just . CTyp $ τ
+    return τ
   canon aΓ τ@(NETHole u τ') = do
     κ <- lookupH aΓ u
-    Just . CTyp $ τ
+    return τ
   canon aΓ (Tλ t κ τ) = do
     ωκ <- canon aΓ κ
-    Just . CTyp $ Tλ t (getKnd ωκ) τ
+    return $ Tλ t ωκ τ
   canon aΓ (TAp τ1 τ2) = do
     ωτ1 <- canon aΓ τ1
     ωτ2 <- canon aΓ τ2
     -- check κ (subkinding)
-    case getTyp ωτ1 of
-      Tλ t κ τ -> wfak aΓ (getTyp ωτ2) κ &>> canon aΓ (subst (getTyp ωτ2) t τ)
-      _ -> trace ("Can't β-reduce " ++ (show $ TAp (getTyp ωτ1) (getTyp ωτ2))) $ Nothing
+    case ωτ1 of
+      Tλ t κ τ -> wfak aΓ ωτ2 κ |>> canon aΓ (subst ωτ2 t τ)
+      _ -> trace ("Can't β-reduce " ++ (show $ TAp ωτ1 ωτ2)) Nothing
 
 -- need a canonical form to normalize higher order singletons
 instance Canon Knd where
-  newtype C Knd = CKnd{getKnd :: Knd}
-                  deriving (Eq, Show, Rewrite)
   canon aΓ Type = return Type
   canon aΓ KHole = return KHole
   canon aΓ (S κ τ) = do
@@ -80,47 +107,6 @@ instance Canon Knd where
     ωκ1 <- canon aΓ κ1
     ωκ2 <- canon (aΓ ⌢ (t, κ1)) κ2
     Just $ Π t ωκ1 ωκ2
-
-ηExpand :: _
-ηExpand = undefined
-
-tequiv :: Ctx -> Typ -> Typ -> Knd -> Bool
-tequiv aΓ τ1 τ2 κ =
-  isJust
-    (do ωτ1 <- canon aΓ τ1
-        ωτ2 <- canon aΓ τ2
-        ωκ <- canon aΓ κ
-        tequiv' aΓ ωτ1 ωτ2 ωκ &>> Just ())
-
-tequiv' :: Ctx -> C Typ -> C Typ -> Knd -> Bool
-tequiv' _ Bse Bse Type = True
-tequiv' aΓ (ωτ1 :⊕ ωτ2) (ωτ3 :⊕ ωτ4) Type =
-  (tequiv aΓ ωτ1 ωτ3 Type) && (tequiv aΓ ωτ2 ωτ4 Type)
-tequiv' aΓ (ETHole u) (ETHole u') κ = isJust (do u == u' &>> lookupH aΓ u)
-tequiv' aΓ (NETHole u1 τ1) (NETHole u2 τ2) κ =
-  isJust
-    (do _ <- u1 == u2 &>> lookupH aΓ u1
-        assert (τ1 ≡ τ2) $ Just ())
-tequiv' aΓ ωτ1@(Tλ _ _ _) ωτ2@(Tλ _ _ _) (Π t κ1 κ2) =
-  tequiv (aΓ ⌢ (t, κ1)) (TAp ωτ1 $ TVar t) (TAp ωτ2 $ TVar t) κ2
-tequiv' aΓ ωτ1 ωτ2 (S ωκ ωτ3) = tequiv aΓ ωτ1 ωτ2 ωκ && tequiv aΓ ωτ1 ωτ3 ωκ
-tequiv' _ _ _ _ = False
-
-kequiv :: Ctx -> Knd -> Knd -> Bool
-kequiv aΓ κ1 κ2 =
-  isJust
-    (do ωκ1 <- canon aΓ κ1
-        ωκ2 <- canon aΓ κ2
-        kequiv' aΓ ωκ1 ωκ2 &>> Just ())
-
-kequiv' :: Ctx -> Knd -> Knd -> Bool
-kequiv' aΓ κ@(Π t _ _) κ'@(Π t' _ _) =
-  let t'' = fresh2 t t'
-   in let (Π _ κ1 κ2) = αRename t'' t κ
-       in let (Π _ κ3 κ4) = αRename t'' t' κ'
-           in (kequiv aΓ κ1 κ3) && (kequiv (aΓ ⌢ (t'', κ1)) κ2 κ4)
-kequiv' aΓ (S κ1 τ1) (S κ2 τ2) = (kequiv aΓ κ1 κ2) && (tequiv aΓ τ1 τ2 κ1)
-kequiv' _ κ1 κ2 = κ1 ≡ κ2
 
 pk :: Ctx -> Typ -> Maybe Knd
 pk aΓ τ = do
