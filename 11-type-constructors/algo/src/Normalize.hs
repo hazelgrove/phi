c3b61037906e94aa609332d0dc07665689a640f8
hejohns <hejohns@umich.edu>
2022-03-27 03:15:15 -0400

    a little wh/normal + hindent

--- 11-type-constructors/algo/src/Normalize.hs
+++ 11-type-constructors/algo/src/Normalize.hs
@@ -25,7 +25,7 @@ fixKnd' _ E.Type = return Type
 fixKnd' _ E.KHole = return KHole
 fixKnd' iΓ (E.S eκ eτ) = do
   iτ <- fixKnd' iΓ eκ
-  iδ <- (ana_elab' iΓ eτ iτ).term -- shouldn't have holes in initial ctx
+  AER {term = iδ} <- ana_elab' iΓ eτ iτ -- shouldn't have holes in initial ctx
   return $ S iτ iδ
 fixKnd' iΓ (E.Π t eκ1 eκ2) = do
   iτ1 <- fixKnd' iΓ eκ1
@@ -59,16 +59,16 @@ syn_elab' iΓ (E.TVar t)
   return $ SER (S iτ $ TVar t) (TVar t) iΓ
 syn_elab' iΓ E.Bse = return $ SER (S Type Bse) Bse iΓ
 syn_elab' iΓ (eτ1 E.:⊕ eτ2) = do
-  AER{term = iδ1, ..} <- ana_elab' iΓ eτ1 Type
-  AER{term = iδ2, ..} <- ana_elab' iΓ eτ2 Type
+  AER {term = iδ1, ..} <- ana_elab' iΓ eτ1 Type
+  AER {term = iδ2, ..} <- ana_elab' iΓ eτ2 Type
   return $ SER (S Type $ iδ1 :⊕ iδ2) (iδ1 :⊕ iδ2) iΓ
 syn_elab' iΓ (E.ETHole u) =
   assert (isNothing $ lookupH iΓ u) $
   return $ SER (S KHole $ ETHole u) (ETHole u) (iΓ :⌢⌢ (u, KHole))
 syn_elab' iΓ (E.NETHole u eτ) =
   assert (isNothing $ lookupH iΓ u) $ do
-  SER {term = iδ, ..} <- syn_elab' iΓ eτ
-  return $ SER (S KHole $ NETHole u iδ) (NETHole u iδ) (iΓ :⌢⌢ (u, KHole))
+    SER {term = iδ, ..} <- syn_elab' iΓ eτ
+    return $ SER (S KHole $ NETHole u iδ) (NETHole u iδ) (iΓ :⌢⌢ (u, KHole))
 syn_elab' iΓ (E.Tλ t eκ eτ) = do
   iτ1 <- fixKnd' iΓ eκ
   SER {typ = iτ2, term = iδ, ..} <- syn_elab' (iΓ :⌢ (t, iτ1)) eτ
@@ -76,8 +76,8 @@ syn_elab' iΓ (E.Tλ t eκ eτ) = do
 syn_elab' iΓ (E.TAp eτ1 eτ2) = do
   SER {typ = iτ1} <- syn_elab' iΓ eτ1 -- we don't have a plain syn
   MPKR {..} <- iΓ |- (iτ1 ⊳→)
-  AER{term = iδ1, ..} <- ana_elab' iΓ eτ1 (Π tπ iτIn iτOut)
-  AER{term = iδ2, ..} <- ana_elab' iΓ eτ2 (iτIn)
+  AER {term = iδ1, ..} <- ana_elab' iΓ eτ1 (Π tπ iτIn iτOut)
+  AER {term = iδ2, ..} <- ana_elab' iΓ eτ2 (iτIn)
   return $ SER (subst iδ2 tπ iτOut) (TAp iδ1 iδ2) iΓ
 
 data AnaElabResult =
@@ -89,25 +89,48 @@ data AnaElabResult =
 -- NOTE: prob don't need ana_elab :: ECtx.Ctx
 -- TODO: something something not holes
 ana_elab' :: Ctx -> E.Typ -> Typ -> Maybe AnaElabResult
-ana_elab' iΓ (E.ETHole u) iτ = assert (isNothing $ lookupH iΓ u) $
-  return $ AER (ETHole u) (iΓ :⌢⌢ (u, iτ))
-ana_elab' iΓ (E.NETHole u eτ) iτ = assert (isNothing $ lookupH iΓ u) $ do
-  SER{term = iδ, ..} <- syn_elab' iΓ eτ
-  return $ AER (NETHole u iδ) (iΓ :⌢⌢ (u, iτ))
+ana_elab' iΓ (E.ETHole u) iτ =
+  assert (isNothing $ lookupH iΓ u) $ return $ AER (ETHole u) (iΓ :⌢⌢ (u, iτ))
+ana_elab' iΓ (E.NETHole u eτ) iτ =
+  assert (isNothing $ lookupH iΓ u) $ do
+    SER {term = iδ, ..} <- syn_elab' iΓ eτ
+    return $ AER (NETHole u iδ) (iΓ :⌢⌢ (u, iτ))
 ana_elab' iΓ eτ iτ = do
   SER {typ = iτ', term = iδ, ..} <- syn_elab' iΓ eτ
   (iΓ |- (iτ' ≲ iτ)) &>> (return $ AER iδ iΓ)
 
+-- TODO: We don't need this, right?
+{-
 τ_elab :: Ctx -> E.Knd -> Maybe Typ
 τ_elab = undefined
-
+-}
 wh_reduc = undefined
 
+wh_normal :: Ctx -> Term -> Term
 wh_normal = undefined
 
-term_normal = undefined
+term_normal :: Ctx -> Term -> Typ -> Term
+term_normal iΓ δ τ =
+  let ωτ = type_normal iΓ τ
+   in case ωτ of
+        Type -> wh_normal
 
-type_normal = undefined
+type_normal :: Ctx -> Typ -> Typ
+type_normal _ Type = Type
+type_normal _ KHole = KHole
+type_normal iΓ (S τ δ) =
+  let ωτ = type_normal iΓ τ
+   in let ωδ = term_normal iΓ δ ωτ
+       in case ωτ of
+            Type -> S Type ωδ
+            KHole -> S KHole ωδ
+            S τ' δ' -> S τ' δ'
+            Π t τ1 τ2 ->
+              let t' = fresh t
+               in let τ3 = αRename t' t τ2
+                   in let ωδ' =
+                            term_normal (iΓ :⌢ (t', τ1)) (TAp ωδ (TVar t')) τ3
+                       in Π t' τ1 (S τ3 ωδ')
 
 tequiv :: ECtx.Ctx -> E.Typ -> E.Typ -> E.Knd -> Bool
 tequiv = undefined
